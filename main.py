3b9a9ec5f4f8eed60d598a6a6d4418da640de8c1
Loeris <fly.rev1ng@gmail.com>
2023-04-04 15:26:10 +0300

    Выделение функций работы с изображениями в отдельный модуль

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
-"""Этот модуль реализует основной функционал программы"""
+"""Этот модуль реализует функционал работы с сайтом"""
 from flask import Flask, render_template, request
-from PIL import Image
 from random import randint
 import webbrowser
+from ImageForge import *
 
 webbrowser.open('http://127.0.0.1:5000')
-
-
-def make_image(crop_values):
-    """Эта функция принимает массив параметров персонажа и сохраняет изображение
-    готового персонажа в файл static/img/image.png
-
-    Она использует файл static/img/Parts.png и вырезает из него выбранные части персонажа"""
-    img = Image.open("static/img/Parts.png")
-    paste_img = img.crop((crop_values[0] * 15, 0, (crop_values[0] + 1) * 15, 20))
-    for i in range(1, len(crop_values)):
-        cropped_img = img.crop((crop_values[i] * 15, i * 20, (crop_values[i] + 1) * 15, (i + 1) * 20))
-        paste_img.paste(cropped_img, (0, 0), mask=cropped_img.split()[3])
-    paste_img.save("static/img/image.png")
-    img.close()
-
-
-def rotate_value(current, maximum, bump=1):
-    """Эта функция возвращает увеличенное значение, имитируя цикличность: 0->1->2->3->0"""
-    return (current + bump) % maximum
-
 
 # Инициализация
 character_stats = [0, 0, 0, 0]
