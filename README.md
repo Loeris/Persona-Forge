9eec68ef376ba70f288f15ee70fb308e12942389
Loeris <fly.rev1ng@gmail.com>
2023-04-04 15:17:40 +0300

    +Документация ~README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Persona-Forge
-Repository for Character Image Creation App
+Description: Repository for Character Image Creation App
+
+Dependencies:
+1. Flask
+2. Pillow
+
+Run command:
+`python main.py`
--- main.py
+++ main.py
@@ -1,39 +1,54 @@
+"""Этот модуль реализует основной функционал программы"""
 from flask import Flask, render_template, request
 from PIL import Image
 from random import randint
+import webbrowser
 
-img = Image.open("static/img/Parts.png")
-# Создаёт изображение персонажа на основе параметров его составляющих
+webbrowser.open('http://127.0.0.1:5000')
+
+
 def make_image(crop_values):
+    """Эта функция принимает массив параметров персонажа и сохраняет изображение
+    готового персонажа в файл static/img/image.png
+
+    Она использует файл static/img/Parts.png и вырезает из него выбранные части персонажа"""
+    img = Image.open("static/img/Parts.png")
     paste_img = img.crop((crop_values[0] * 15, 0, (crop_values[0] + 1) * 15, 20))
     for i in range(1, len(crop_values)):
         cropped_img = img.crop((crop_values[i] * 15, i * 20, (crop_values[i] + 1) * 15, (i + 1) * 20))
         paste_img.paste(cropped_img, (0, 0), mask=cropped_img.split()[3])
     paste_img.save("static/img/image.png")
+    img.close()
 
-# Имитирует расположение чисел в цикле: 0->1->2->3->0
+
 def rotate_value(current, maximum, bump=1):
+    """Эта функция возвращает увеличенное значение, имитируя цикличность: 0->1->2->3->0"""
     return (current + bump) % maximum
 
-# Initiation
-crop_values = [0, 0, 0, 0]
-make_image(crop_values)
+
+# Инициализация
+character_stats = [0, 0, 0, 0]
+make_image(character_stats)
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    """Эта функция реализует главную страницу"""
     if request.method == 'POST':
         for act_number in range(4):
             # Кнопки ротации составляющих
             if request.form.get(f'act{act_number}') == '>':
-                crop_values[act_number] = rotate_value(crop_values[act_number], 4)
+                character_stats[act_number] = rotate_value(character_stats[act_number], 4)
             elif request.form.get(f'act{act_number}') == '<':
-                crop_values[act_number] = rotate_value(crop_values[act_number], 4, -1)
+                character_stats[act_number] = rotate_value(character_stats[act_number], 4, -1)
+            elif request.form.get(f'act{act_number}') == '?':
+                character_stats[act_number] = randint(0, 3)
     elif request.method == 'GET':
         return render_template('index.html')
-    make_image(crop_values)
+    make_image(character_stats)
 
     return render_template("index.html")
+
 
 app.run()
